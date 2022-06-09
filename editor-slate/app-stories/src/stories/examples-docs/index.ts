1ffc4153808619b08a88829cda20f09680199868
uptonking <jinyaoo86@gmail.com>
2022-06-09 15:06:42 +0800

    feat: nested contenteditable keydown event

--- editor-slate/app-stories/src/stories/examples-docs/index.ts
+++ editor-slate/app-stories/src/stories/examples-docs/index.ts
@@ -5,3 +5,4 @@ export { SlateCommandsApp as A14SlateCommandsApp } from './a1-starter/a14-comman
 export { MentionApp as A21MentionApp } from './a2-features/a21-mention';
 export { InlineToolbarApp as A22InlineToolbarApp } from './a2-features/a22-inline-toolbar';
 export { InlineLinkButtonApp as A23InlineLinkButtonApp } from './a2-features/a23-inline-link-button';
+export { SlateWrappedInContenteditableApp as A71InlineLinkButtonApp } from './a7-issues/a71-slate-wrapped-in-contenteditable';
